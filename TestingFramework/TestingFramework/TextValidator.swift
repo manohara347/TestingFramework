f4e069fc38d7218a9920515c62319c137377b678
Manohara <manoharakle@gmail.com>
2023-04-22 09:55:45 +0530

    removed Empty folder

---
+++ TestingFramework/TestingFramework/TextValidator.swift
@@ -0,0 +1,14 @@
+//
+//  TextValidator.swift
+//  TestingFramework
+//
+//  Created by Manoharreddy on 22/04/23.
+//
+
+import Foundation
+
+
+public func testValidation() {
+    print("Hello")
+}
+
